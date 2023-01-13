3d9ad0f70df86604863c918888b43ee98b3a5269
TIMOTHE GUILLAUME LI <timothe.2.guillaume-li@atos.net>
2023-01-13 15:44:49 +0100

    first modifications

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -1,58 +1,89 @@
 from sklearn.base import BaseEstimator
 from sklearn.pipeline import make_pipeline
-from sklearn.linear_model import LogisticRegression
+# from sklearn.linear_model import LogisticRegression
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
 
+class FeatureExtractor(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
 
-class FeatureExtractor(BaseEstimator):
-
-    def fit(self, X, y):
+    def fit(self, X_df, y):
         return self
 
+    def transform(self, X_df):
+        X_df_new = X_df.copy()
+        X_df_new = compute_rolling_std(X_df_new, 'Beta', '2h')
+        return X_df_new
+
+class FeatureReductor():
+    def __init__(self, num_features):
+        self.num_features = num_features
+        self.selector = SelectKBest(mutual_info_classif, k=self.num_features)
+    
+    def fit(self, X_train, y_train):
+        self.selector.fit(X_train, y_train)
+    
     def transform(self, X):
-        return compute_rolling_std(X, 'Beta', '2h')
+        return self.selector.transform(X)
+    
+    def fit_transform(self, X_train, y_train):
+        self.fit(X_train, y_train)
+        return self.transform(X_train)
+
 
 
 class Classifier(BaseEstimator):
-
     def __init__(self):
-        self.model = LogisticRegression(max_iter=1000)
+        self.model = RandomForestClassifier(n_estimators = 100, n_jobs = -1)
 
     def fit(self, X, y):
         self.model.fit(X, y)
-
+        
     def predict(self, X):
         y_pred = self.model.predict_proba(X)
-        return y_pred
+        y_smooth = pd.Series(y_pred[:, 1]).rolling(12, min_periods=0, center=True).quantile(0.9)
+        y_out = np.array([1-y_smooth, y_smooth]).T
+        return y_out
+        
+    def predict_proba(self, X):
+        return self.model.predict_proba(X)
 
 
 def get_estimator():
 
     feature_extractor = FeatureExtractor()
 
+    feature_reductor = FeatureReductor()
+
+    standard_scaler = StandardScaler()
+
     classifier = Classifier()
 
-    pipe = make_pipeline(feature_extractor, classifier)
+    pipe = make_pipeline(feature_extractor, feature_reductor, 
+                         standard_scaler, classifier)
     return pipe
 
 
-def compute_rolling_std(X_df, feature, time_window, center=False):
+def compute_rolling_std(data, feature, time_window, center=False):
     """
     For a given dataframe, compute the standard deviation over
     a defined period of time (time_window) of a defined feature
 
     Parameters
     ----------
-    X : dataframe
+    data : dataframe
     feature : str
-        feature in the dataframe we wish to compute the rolling std from
-    time_window : str
+        feature in the dataframe we wish to compute the rolling mean from
+    time_indow : str
         string that defines the length of the time window passed to `rolling`
     center : bool
         boolean to indicate if the point of the dataframe considered is
         center or end of the window
     """
     name = '_'.join([feature, time_window, 'std'])
-    X_df[name] = X_df[feature].rolling(time_window, center=center).std()
-    X_df[name] = X_df[name].ffill().bfill()
-    X_df[name] = X_df[name].astype(X_df[feature].dtype)
-    return X_df
+    data[name] = data[feature].rolling(time_window, center=center).std()
+    data[name] = data[name].ffill().bfill()
+    data[name].astype(data[feature].dtype)
+    return data
