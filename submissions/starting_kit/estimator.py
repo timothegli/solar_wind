944d32daf17578c52928fc4a0055263b53e2b4c6
TIMOTHE GUILLAUME LI <timothe.2.guillaume-li@atos.net>
2023-01-13 16:23:00 +0100

    correction of syntax errors

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -1,11 +1,15 @@
 from sklearn.base import BaseEstimator
 from sklearn.pipeline import make_pipeline
-# from sklearn.linear_model import LogisticRegression
+from sklearn.base import TransformerMixin
 from sklearn.feature_selection import SelectKBest, mutual_info_classif
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import numpy as np
+
 
 class FeatureExtractor(BaseEstimator, TransformerMixin):
+
     def __init__(self):
         pass
 
@@ -17,7 +21,9 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
         X_df_new = compute_rolling_std(X_df_new, 'Beta', '2h')
         return X_df_new
 
+
 class FeatureReductor():
+
     def __init__(self, num_features):
         self.num_features = num_features
         self.selector = SelectKBest(mutual_info_classif, k=self.num_features)
@@ -33,17 +39,17 @@ class FeatureReductor():
         return self.transform(X_train)
 
 
-
 class Classifier(BaseEstimator):
     def __init__(self):
-        self.model = RandomForestClassifier(n_estimators = 100, n_jobs = -1)
+        self.model = RandomForestClassifier(n_estimators=100, n_jobs=-1)
 
     def fit(self, X, y):
         self.model.fit(X, y)
         
     def predict(self, X):
         y_pred = self.model.predict_proba(X)
-        y_smooth = pd.Series(y_pred[:, 1]).rolling(12, min_periods=0, center=True).quantile(0.9)
+        y_smooth = pd.Series(y_pred[:, 1]).\
+            rolling(12, min_periods=0, center=True).quantile(0.9)
         y_out = np.array([1-y_smooth, y_smooth]).T
         return y_out
         
@@ -55,7 +61,7 @@ def get_estimator():
 
     feature_extractor = FeatureExtractor()
 
-    feature_reductor = FeatureReductor()
+    feature_reductor = FeatureReductor(5)
 
     standard_scaler = StandardScaler()
 
